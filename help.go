b21aac6bb28de4b9a9178219fb5e4851addf9d17
Pete Davison <pd93.uk@outlook.com>
2022-10-01 22:45:54 +0000

    feat: add aliases to --list and --list-all flags

--- help.go
+++ help.go
@@ -44,7 +44,8 @@ func (e *Executor) printTasks(listAll bool) {
 	// Format in tab-separated columns with a tab stop of 8.
 	w := tabwriter.NewWriter(e.Stdout, 0, 8, 0, '\t', 0)
 	for _, task := range tasks {
-		fmt.Fprintf(w, "* %s: \t%s\n", task.Name(), task.Desc)
+		taskNames := append([]string{task.Name()}, task.Aliases...)
+		fmt.Fprintf(w, "* %s: \t%s\n", strings.Join(taskNames, "|"), task.Desc)
 	}
 	w.Flush()
 }
